8e1d01fee786bceb1c247bfd7ae3100e09f37099
Paul Fidika <paul@fidika.com>
2024-04-20 15:51:29 -0600

    css finally appears in graph-editor library

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-
-import GraphEditor from '@comfy-creator/graph-editor';
-
 import './index.css';
+import GraphEditor from '@comfy-creator/graph-editor';
+// import '@comfy-creator/graph-editor/dist/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
